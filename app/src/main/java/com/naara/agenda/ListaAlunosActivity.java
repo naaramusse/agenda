4aad22c47abfd4478f9aa786c3b1b2570c5412b3
Naara <naara.musse@simetrianet.com.br>
2017-02-06 09:25:55 -0300

    atividade 1 e 2

---
+++ app/src/main/java/com/naara/agenda/ListaAlunosActivity.java
@@ -0,0 +1,23 @@
+package com.naara.agenda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class ListaAlunosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_alunos);
+
+        ListView listaAlunos = (ListView) findViewById(R.id.lista_alunos);
+
+        String[] alunos = {"Daniel", "Ronaldo", "Jeferson", "Felipe"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos);
+
+        listaAlunos.setAdapter(adapter);
+    }
+}
