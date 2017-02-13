8da287df2e815b44eca8075316e1b64fd73b7b61
Naara <naara.musse@simetrianet.com.br>
2017-02-13 16:45:44 -0300

    finalizado atividade 6

--- app/src/main/java/com/naara/agenda/ListaAlunosActivity.java
+++ app/src/main/java/com/naara/agenda/ListaAlunosActivity.java
@@ -1,6 +1,11 @@
 package com.naara.agenda;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ContextMenu;
@@ -18,9 +23,12 @@ import com.naara.agenda.modelo.Aluno;
 import java.lang.Override;
 import java.util.List;
 
+
 public class ListaAlunosActivity extends AppCompatActivity {
 
     ListView listaAlunos;
+
+    public int PERMISSION_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,6 +87,11 @@ public class ListaAlunosActivity extends AppCompatActivity {
         super.onCreateContextMenu(menu, v, menuInfo);
 
         MenuItem del = menu.add("Deletar");
+        MenuItem visitarSite = menu.add("Visitar site");
+        MenuItem ligar = menu.add("Ligar para aluno");
+        MenuItem sms = menu.add("Mandar sms");
+        MenuItem mapa = menu.add("Achar no mapa");
+
 
         AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
 
@@ -88,6 +101,7 @@ public class ListaAlunosActivity extends AppCompatActivity {
 
         final String nome = aluno.getNome();
 
+
         del.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
@@ -95,10 +109,104 @@ public class ListaAlunosActivity extends AppCompatActivity {
                 dao.deleta(aluno);
                 dao.close();
                 carregaLista();
-                return false;
+                return true;
             }
         });
 
+        visitarSite.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
 
+                if (aluno.getSite() != null){
+                    intent.setData(Uri.parse(aluno.getSite()));
+                }
+
+                startActivity(intent);
+
+                return true;
+            }
+        });
+
+        sms.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener(){
+
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+
+                if (aluno.getTelefone() != null){
+                    intent.setData(Uri.parse("sms:"+ aluno.getTelefone()));
+                }
+
+                startActivity(intent);
+
+                return true;
+            }
+        });
+
+        mapa.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener(){
+
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+
+                if (aluno.getEndereco() != null){
+                    intent.setData(Uri.parse("geo:0,0?q="+ aluno.getEndereco()));
+                }
+
+                startActivity(intent);
+
+                return true;
+            }
+        });
+
+        ligar.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener(){
+
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Intent intent = new Intent(Intent.ACTION_CALL);
+
+                if (ContextCompat.checkSelfPermission(ListaAlunosActivity.this, Manifest.permission.CALL_PHONE)
+                        != PackageManager.PERMISSION_GRANTED) {
+
+                    ActivityCompat.requestPermissions(ListaAlunosActivity.this, new String[]{Manifest.permission.CALL_PHONE}, 1);
+                }else{
+
+                    if (aluno.getTelefone() != null){
+                        intent.setData(Uri.parse("tel:"+ aluno.getTelefone()));
+                    }
+
+                    startActivity(intent);
+                }
+
+
+                return true;
+            }
+        });
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult (int requestCode,
+                                            String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    Toast.makeText(ListaAlunosActivity.this, "Aluno ", Toast.LENGTH_SHORT).show();
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 }
